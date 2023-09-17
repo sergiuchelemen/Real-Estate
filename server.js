545aed7691554168cca7d57c27f54f7f3d77cd9d
Sergiu <schelemen90@gmail.com>
2023-09-17 20:57:45 +0300

    asdasd

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+app.use(express.static("views"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
 
 app.listen(3000);
